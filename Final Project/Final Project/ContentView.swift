e00a6ac5d57a27e73839329d7d99b071f9ce8e5b
Tim Allen <allen079@rangers.uwp.edu>
2025-04-02 16:07:16 -0500

    Created Project in Xcode

---
+++ Final Project/Final Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final Project
+//
+//  Created by Timothy R Allen on 4/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

a74d8e24d292afe0212defe46d6acf555f6bfad2
Tim Allen <allen079@rangers.uwp.edu>
2025-04-02 16:45:20 -0500

    Implemented a basic 9x9 grid

---
+++ Final Project/Final Project/SudokuView.swift
@@ -0,0 +1,66 @@
+//
+//  SudokuView.swift
+//  Final Project
+//
+//  Created by Timothy R Allen on 4/2/25.
+//
+
+import SwiftUI
+
+struct SudokuView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        Section()
+        Section()
+        Section()
+      }
+      HStack {
+        Section()
+        Section()
+        Section()
+      }
+      HStack {
+        Section()
+        Section()
+        Section()
+      }
+    }
+    .scaledToFit()
+  }
+}
+
+struct Section: View {
+  var body: some View{
+    VStack {
+      HStack {
+        Square().padding(-5)
+        Square().padding(-5)
+        Square().padding(-5)
+      }
+      HStack {
+        Square().padding(-5)
+        Square().padding(-5)
+        Square().padding(-5)
+      }
+      HStack {
+        Square().padding(-5)
+        Square().padding(-5)
+        Square().padding(-5)
+      }
+    }
+  }
+}
+
+struct Square: View {
+  var body: some View{
+    Rectangle()
+      .frame(width: 50.0, height: 50.0)
+      .foregroundColor(.white)
+      .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  SudokuView()
+}

e7cff500a3d545c0dfb8c9604f931b99bebe953b
Tim Allen <allen079@rangers.uwp.edu>
2025-04-09 15:07:01 -0500

    Added variables to grid squares, cleaned up grid generation.

--- Final Project/Final Project/SudokuView.swift
+++ Final Project/Final Project/SudokuView.swift
@@ -9,56 +9,32 @@ import SwiftUI
 
 struct SudokuView: View {
   var body: some View {
-    VStack {
-      HStack {
-        Section()
-        Section()
-        Section()
-      }
-      HStack {
-        Section()
-        Section()
-        Section()
-      }
-      HStack {
-        Section()
-        Section()
-        Section()
+    Grid{
+      ForEach(0..<9){n in
+        GridRow{
+          ForEach(0..<9){i in
+            Square(column: i, row: 0, section: Int((i / 3) + ((n / 3)*3)))
+          }
+        }
       }
     }
-    .scaledToFit()
-  }
-}
-
-struct Section: View {
-  var body: some View{
-    VStack {
-      HStack {
-        Square().padding(-5)
-        Square().padding(-5)
-        Square().padding(-5)
-      }
-      HStack {
-        Square().padding(-5)
-        Square().padding(-5)
-        Square().padding(-5)
-      }
-      HStack {
-        Square().padding(-5)
-        Square().padding(-5)
-        Square().padding(-5)
-      }
-    }
+    Spacer()
   }
 }
 
 struct Square: View {
+  var column: Int = 0
+  var row: Int = 0
+  var section: Int = 0
+  var number: Int = 0
+  
   var body: some View{
-    Text("1")
+    Text(String(number))
       .foregroundColor(.black)
       .frame(width: 45.0, height: 45.0)
       .foregroundColor(.white)
       .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+      .padding(-5)
   }
 }
 

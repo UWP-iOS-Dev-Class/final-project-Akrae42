fe1ef7c86076597eb7bc3db8fcfcbcb585e9e071
Tim Allen <allen079@rangers.uwp.edu>
2025-04-09 16:01:26 -0500

    minor grid changes

--- Final Project/Final Project/SudokuView.swift
+++ Final Project/Final Project/SudokuView.swift
@@ -13,7 +13,7 @@ struct SudokuView: View {
       ForEach(0..<9){n in
         GridRow{
           ForEach(0..<9){i in
-            Square(column: i, row: 0, section: Int((i / 3) + ((n / 3)*3)))
+            Square(column: i, row: 0, section: Int((i / 3) + ((n / 3)*3)), number: n+i)
           }
         }
       }
@@ -26,15 +26,25 @@ struct Square: View {
   var column: Int = 0
   var row: Int = 0
   var section: Int = 0
-  var number: Int = 0
+  @State var number: Int = 0
   
   var body: some View{
-    Text(String(number))
-      .foregroundColor(.black)
-      .frame(width: 45.0, height: 45.0)
-      .foregroundColor(.white)
-      .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-      .padding(-5)
+    if (number == 0){
+      Text("")
+        .foregroundColor(.black)
+        .frame(width: 45.0, height: 45.0)
+        .foregroundColor(.white)
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+        .padding(-5)
+    }
+    else {
+      Text(String(number))
+        .foregroundColor(.black)
+        .frame(width: 45.0, height: 45.0)
+        .foregroundColor(.white)
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+        .padding(-5)
+    }
   }
 }
 
